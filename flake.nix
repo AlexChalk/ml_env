a54ae7abba862c936362c35d076ae6fde4d07a48
Alex Chalk <contact@alexchalk.net>
2024-06-02 15:26:43 -0400

    Add custom jupyter and python builds

--- flake.nix
+++ flake.nix
@@ -3,21 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, poetry2nix }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv overrides;
-    in
-    {
-      packages.${system} = {
-        bear-classifier = mkPoetryEnv {
+  outputs = { self, nixpkgs, poetry2nix, flake-utils }:
+    flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux ] (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv overrides;
+        python = mkPoetryEnv {
           projectDir = self;
           preferWheels = false;
           overrides = overrides.withDefaults (final: prev: {
@@ -39,26 +38,46 @@
             );
           });
         };
-        default = self.packages.${system}.bear-classifier;
-      };
-
-      devShells.${system} = {
-        # Shell for app dependencies.
-        #
-        #     nix develop
-        #
-        # Use this shell for developing your app.
-        default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.bear-classifier ];
+        # This name makes it the default kernel, any other (e.g. pythonml) preserves
+        # the nixpkgs default python kernel
+        definitions = {
+          python3 = {
+            displayName = "Python ML";
+            language = "python";
+            logo32 = "${pkgs.jupyter.sitePackages}/ipykernel/resources/logo-32x32.png";
+            logo64 = "${pkgs.jupyter.sitePackages}/ipykernel/resources/logo-64x64.png";
+            argv = [
+              "${python}/bin/python"
+              "-m"
+              "ipykernel_launcher"
+              "-f"
+              "{connection_file}"
+            ];
+          };
         };
-        # Shell for poetry.
-        #
-        #     nix develop .#poetry
-        #
-        # Use this shell for changes to pyproject.toml and poetry.lock.
-        poetry = pkgs.mkShell {
-          packages = [ pkgs.poetry ];
+        jupyter = pkgs.jupyter.override { inherit definitions; };
+      in
+      {
+        packages = { inherit python jupyter; };
+        packages.default = jupyter;
+        apps.default = {
+          type = "app";
+          program = "${jupyter}/bin/jupyter";
         };
-      };
-    };
+        apps.python = {
+          type = "app";
+          program = "${python}/bin/python";
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            python
+            jupyter
+            pkgs.python3.pkgs.jupyterlab
+          ];
+          shellHook = ''
+            CUSTOM_NIXSHELL=pythonml ${pkgs.zsh}/bin/zsh; exit
+          '';
+        };
+      }
+    );
 }
