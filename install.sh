1262c7dac96028923046504aac26b1afbce249df
Alex Chalk <contact@alexchalk.net>
2025-11-26 15:23:51 -0500

    Improve vast.ai setup process

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 # curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --init none --no-confirm
 
 # Pytorch (Vast) Template
-#ssh -p 46974 root@213.181.123.90 -L 8080:localhost:8080
+# ssh -p 21555 root@213.181.123.20 -L 8080:localhost:8080
 
-# rsync -avz -e "ssh -p 46441" --exclude-from='/home/adc/ml_env/.rsyncignore' /home/adc/ml_env/ root@38.247.78.2:/workspace/ml_env
+# rsync -avz -e "ssh -p 21555" --exclude-from='/home/adc/ml_env/.rsyncignore' /home/adc/ml_env/ root@213.181.123.20:/workspace/ml_env
 
-# rsync -avz -e "ssh -p 46441" root@38.247.78.2:/workspace/ml_env/miniai/ /home/adc/ml_env/miniai
-# rsync -avz -e "ssh -p 46441" root@38.247.78.2:/workspace/ml_env/nbs/14_augment.ipynb /home/adc/ml_env/nbs/14_augment.ipynb
-# rsync -avz -e "ssh -p 46441" root@38.247.78.2:/workspace/ml_env/nbs/models/ /home/adc/ml_env/nbs/models
+# rsync -avz -e "ssh -p 21555" root@213.181.123.20:/workspace/ml_env/miniai/ /home/adc/ml_env/miniai
+# rsync -avz -e "ssh -p 21555" root@213.181.123.20:/workspace/ml_env/nbs/14_augment.ipynb /home/adc/ml_env/nbs/14_augment.ipynb
+# rsync -avz -e "ssh -p 21555" root@213.181.123.20:/workspace/ml_env/nbs/models/ /home/adc/ml_env/nbs/models
 
 export PYTHON_VERSION=3.12
 export PYTORCH_CUDA_VERSION=12.4
@@ -17,5 +17,6 @@ mamba create -y -n ml -c fastai -c pytorch -c nvidia -c conda-forge python="$PYT
 mamba activate ml
 mamba install -y -c fastai -c pytorch -c nvidia -c conda-forge pytorch pytorch-cuda="$PYTORCH_CUDA_VERSION" rise opencv pytables fastai gradio watchfiles nbdev numpy ipykernel ipywidgets pandas matplotlib lxml beautifulsoup4 html5lib openpyxl requests sqlalchemy seaborn scipy statsmodels patsy scikit-learn pyarrow numba timm fastkaggle gast transformers[torch] py7zr diffusers datasets # tensorflow
 pip install -Uq torcheval
-pip install ./ml_env/miniai
+rm ./ml_env/pyproject.toml
+pip install -e ./ml_env
 python -m ipykernel install --user --name ml --display-name "Python (ML)"
