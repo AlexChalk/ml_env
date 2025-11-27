1777d3c4e0d4875ff011f609a6b494ea54daddbe
Alex Chalk <contact@alexchalk.net>
2025-11-27 18:23:31 -0500

    Update copy scripts to use vast clipboard format

--- copy-from.sh
+++ copy-from.sh
@@ -1,4 +1,5 @@
-export PASSWORD="$1"
-export ADDRESS="$2"
+INPUT=$(cat)
+PORT=$(echo "$INPUT" | grep -oP '\-p\s+\K\d+')
+ADDRESS=$(echo "$INPUT" | grep -oP '@\K[0-9.]+')
 
-rsync -avz -e "ssh -p $PASSWORD" --exclude-from='/home/adc/ml_env/.rsyncignore' root@"$ADDRESS":/workspace/ml_env/ /home/adc/ml_env
+rsync -avz -e "ssh -p $PORT" --exclude-from='/home/adc/ml_env/.rsyncignore' root@"$ADDRESS":/workspace/ml_env/ /home/adc/ml_env
--- copy-to.sh
+++ copy-to.sh
@@ -1,3 +1,5 @@
-export PASSWORD="$1"
-export ADDRESS="$2"
-rsync -avz -e "ssh -p $PASSWORD" --exclude-from='/home/adc/ml_env/.rsyncignore' /home/adc/ml_env/ root@"$ADDRESS":/workspace/ml_env
+INPUT=$(cat)
+PORT=$(echo "$INPUT" | grep -oP '\-p\s+\K\d+')
+ADDRESS=$(echo "$INPUT" | grep -oP '@\K[0-9.]+')
+
+rsync -avz -e "ssh -p $PORT" --exclude-from='/home/adc/ml_env/.rsyncignore' /home/adc/ml_env/ root@"$ADDRESS":/workspace/ml_env
