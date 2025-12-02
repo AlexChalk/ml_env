017c9611f48519a200eee42e791130ecfd1db70b
Alex Chalk <contact@alexchalk.net>
2025-12-02 11:37:30 -0500

    Add einops to project

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ export PYTORCH_CUDA_VERSION=12.4
 eval "$(mamba shell hook --shell bash)"
 mamba create -y -n ml -c fastai -c pytorch -c nvidia -c conda-forge python="$PYTHON_VERSION"
 mamba activate ml
-mamba install -y -c fastai -c pytorch -c nvidia -c conda-forge pytorch pytorch-cuda="$PYTORCH_CUDA_VERSION" rise opencv pytables fastai gradio watchfiles nbdev numpy ipykernel ipywidgets pandas matplotlib lxml beautifulsoup4 html5lib openpyxl requests sqlalchemy seaborn scipy statsmodels patsy scikit-learn pyarrow numba timm fastkaggle gast transformers[torch] py7zr diffusers datasets accelerate # tensorflow
+mamba install -y -c fastai -c pytorch -c nvidia -c conda-forge pytorch pytorch-cuda="$PYTORCH_CUDA_VERSION" rise opencv pytables fastai gradio watchfiles nbdev numpy ipykernel ipywidgets pandas matplotlib lxml beautifulsoup4 html5lib openpyxl requests sqlalchemy seaborn scipy statsmodels patsy scikit-learn pyarrow numba timm fastkaggle gast transformers[torch] py7zr diffusers datasets accelerate einops # tensorflow
 pip install -Uq torcheval
 rm /workspace/ml_env/pyproject.toml
 pip install -e /workspace/ml_env
